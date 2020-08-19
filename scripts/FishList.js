5110fe43dcb8a079bc138120fb580211e04089f8
Audrey Thomasson <audrey@macbook-pro.lan>
2020-08-19 14:32:11 -0500

    got fish picture working

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -5,6 +5,7 @@
 // TODO: Import `useFish` from the data provider module
 
 import { useFish } from './FishDataProvider.js';
+import { Fish } from "./Fish.js";
 
 export const FishList = () => {
 
@@ -12,10 +13,22 @@ export const FishList = () => {
     const contentElement = document.querySelector(".fishList")
     const fishes = useFish()
 
+    let fishHTMLRepresentation = "";
+    for (const fish of fishes) {
+
+        fishHTMLRepresentation += Fish(fish);
+
+        /*
+            Invoke the Fish component function
+            and pass the current fish object as an argument.
+            Each time, add the return value to the
+            fishHTMLRepresentations variable with `+=`
+        */
+    }
+
     // Add to the existing HTML in the content element
     contentElement.innerHTML += `
-
-            All the fish go here!
+        ${fishHTMLRepresentation}
         
     `
 }
--- scripts/main.js
+++ scripts/main.js
@@ -10,8 +10,5 @@ import { FishList} from './FishList.js';
 const allTheFish = useFish();
 console.log("all the fish", allTheFish);
 
-for (const fish of allTheFish) {
-    console.log("the item is ", fish)
-}
 
 FishList();
